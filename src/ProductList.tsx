0ddcfc7217d766b4d10fefe5b7e18b5d1efcad81
Stan Choi <sjc591@nyu.edu>
2025-03-11 15:02:09 -0400

    See name and image of product in some logical order

--- src/ProductList.tsx
+++ src/ProductList.tsx
@@ -7,6 +7,11 @@ const ProductList = () => {
             const response = await fetch("https://localhost:7068/api/categories/C3F6B090-24DE-4F25-9895-1234567A2BC5/products");
             const data = await response.json();
             console.log(data);
+            data.sort((a, b) => {
+                const numA = parseInt(a.name.match(/\d+/)[0], 10); // Extracts the number from the name
+                const numB = parseInt(b.name.match(/\d+/)[0], 10); // Extracts the number from the name
+                return numA - numB; // Sort numerically
+              });
             setProducts(data);
         }
 
@@ -16,7 +21,8 @@ const ProductList = () => {
     return (
         <ul>
             {products.map(product => {
-                return <li>{product.name}</li>
+                return <><li>{product.name}</li><img src={product.imageUrl} width={50} height={50} /></>
+                
             })}
         </ul>
     )
