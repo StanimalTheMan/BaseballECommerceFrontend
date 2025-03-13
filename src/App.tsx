b7d5fb0970439f5dcfd45e4cc51207bbe7e0379a
Stan Choi <sjc591@nyu.edu>
2025-03-13 18:27:32 -0400

    Add React Router, make initial route for ProductList

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css'
 import ProductList from './ProductList'
 
@@ -8,9 +7,11 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <ProductList />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/products" element={<ProductList/>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
